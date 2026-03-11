a02051d19c8f765e7f12904930adf0019c55ad9e
santos <tabladakristing@gmail.com>
2026-03-11 13:54:06 -0600

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1> hola desde Vite + React</h1>
+      <h1> empezamos el año escolar</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
